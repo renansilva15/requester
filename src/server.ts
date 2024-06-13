ce008ce77f2cddb95f8832b97a9f76e9540578ad
renansilva15 <renangabrielsilva150@gmail.com>
2024-06-12 21:42:35 -0300

    feat: add fastify server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hi!');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', () => {
+  return { status: 'OK' };
+});
+
+app.listen({ port: 3333 }).then((url) => {
+  console.log(`Server listen at ${url}`);
+});
